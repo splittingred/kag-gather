5a9050e165921f8e3822102b3a3bae3b4294e6ab
Shaun McCormick <shaun@modx.com>
2013-04-07 20:43:38 -0500

    documentation update

--- lib/kag/models/user.rb
+++ lib/kag/models/user.rb
@@ -1,5 +1,7 @@
 require 'kag/models/model'
-
+##
+# Abstraction of a User record
+#
 class User < KAG::Model
   has_many :players
   has_many :matches, :through => :players
@@ -8,15 +10,31 @@ class User < KAG::Model
   has_many :user_stats
 
   class << self
+    ##
+    # Create a new User record from a Cinch::User
+    #
+    # @param [Cinch::User] user
+    # @return [Boolean|User]
+    #
     def create(user)
       u = User.new
       u.authname = user.authname
       u.nick = user.nick
       u.host = user.host
       u.created_at = Time.now
-      u.save
+      if u.save
+        u
+      else
+        false
+      end
     end
 
+    ##
+    # Fetch the User object for a authname or user
+    #
+    # @param [String|Symbol|Cinch::User]
+    # @return [User|Boolean]
+    #
     def fetch(user)
       return user if user.class == User
       if user.class == String
@@ -41,24 +59,52 @@ class User < KAG::Model
     end
   end
 
+  ##
+  # See if the user is linked
+  #
+  # @return [Boolean]
+  #
   def linked?
     u.kag_user != '' and !u.kag_user.nil?
   end
 
+  ##
+  # Unlink the user from their KAG account
+  #
+  # @return [Boolean]
+  #
   def unlink
     u.kag_user = ""
     u.save
   end
 
+  ##
+  # Get the stats for the user
+  #
+  # @param [Boolean] bust_cache
+  # @return [Array]
+  #
   def stats(bust_cache = true)
     self.user_stats(bust_cache)
   end
 
+  ##
+  # Get the stats text for the user
+  #
+  # @return [String]
+  #
   def stats_text
     kd_ratio = self.stat("kills").to_s+'/'+self.stat("deaths").to_s
     "#{self.authname} has played in #{self.matches(true).count.to_s} matches, with a K/D ratio of: #{kd_ratio}"
   end
 
+  ##
+  # Get a stat value for a user
+  #
+  # @param [String|Symbol] k
+  # @param [Boolean] return_value
+  # @return [Integer|Float|UserStat]
+  #
   def stat(k,return_value = true)
     s = self.stats.where(:name => k.to_s).first
     if return_value
@@ -72,6 +118,13 @@ class User < KAG::Model
     end
   end
 
+  ##
+  # Set the stat for the user for a key
+  #
+  # @param [String|Symbol] k
+  # @param [Integer|Float|String] v
+  # @return [Boolean]
+  #
   def set_stat(k,v)
     s = self.stat(k,false)
     unless s
@@ -85,6 +138,12 @@ class User < KAG::Model
     s.save
   end
 
+  ##
+  # Delete the stat record for a key
+  #
+  # @param [String|Symbol] k
+  # @return [Boolean]
+  #
   def delete_stat(k)
     s = self.stat(k,false)
     if s
@@ -92,6 +151,13 @@ class User < KAG::Model
     end
   end
 
+  ##
+  # Increase a stat value
+  #
+  # @param [String|Symbol] key
+  # @param [Integer] increment
+  # @return [Boolean]
+  #
   def inc_stat(k,increment = 1)
     s = self.stat(k,false)
     unless s
@@ -105,6 +171,13 @@ class User < KAG::Model
     s.save
   end
 
+  ##
+  # Decrease a stat value
+  #
+  # @param [String|Symbol] key
+  # @param [Integer] decrement
+  # @return [Boolean]
+  #
   def dec_stat(k,decrement = 1)
     s = self.stat(k,false)
     unless s
@@ -118,7 +191,24 @@ class User < KAG::Model
     s.save
   end
 
+  ##
+  # Ignore this user
+  #
+  # @param [Integer] hours
+  # @param [String] reason
+  # @param [User|String|Cinch::User] creator
+  # @return [Boolean]
+  #
   def ignore(hours,reason = '',creator = nil)
     Ignore.them(self.authname,hours,reason,creator)
+  end
+
+  ##
+  # See if this user is ignored
+  #
+  # @return [Boolean]
+  #
+  def ignored?
+    Ignore.is?(self.authname)
   end
 end

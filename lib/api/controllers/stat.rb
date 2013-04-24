45415612ecc937f38c95d4aaa4a3015424ead4b6
Shaun McCormick <shaun@modx.com>
2013-04-23 23:19:13 -0500

    hopeful fix

--- lib/api/controllers/stat.rb
+++ lib/api/controllers/stat.rb
@@ -6,28 +6,28 @@ module KAG
         @@class_key = 'UserStat'
         @@primary_key = :name
 
-        def read(name)
-          limit = @params[:limit] || 20
-          times = ::UserStat.where(:name => name).sum('value')
-          users_count = ::UserStat.select('*,users.kag_user').joins(:user).where(:name => name).count
-          users = ::UserStat.select('*,users.kag_user').joins(:user).where(:name => name).limit(limit).order('value DESC')
-          list = []
-          users.each do |u|
-            list << {
-                :user => u.kag_user,
-                :times => u.value
-            }
+        def get
+          name = @params[:name].to_s || ''
+          if name.empty?
+            self.failure('err_ns')
+          else
+            limit = @params[:limit] || 20
+            times = ::UserStat.where(:name => name).sum('value')
+            users_count = ::UserStat.select('*,users.kag_user').joins(:user).where(:user_stats => {:name => name}).count
+            users = ::UserStat.select('*,users.kag_user').joins(:user).where(:user_stats => {:name => name}).limit(limit).order('value DESC')
+            list = []
+            users.each do |u|
+              list << {
+                  :user => u.kag_user,
+                  :times => u.value
+              }
+            end
+            self.collection(list,users_count,{
+                :name => name,
+                :times => times,
+            })
           end
-          self.collection(list,users_count,{
-              :name => name,
-              :times => times,
-          })
         end
-
-        def list
-          self.failure('err_nf')
-        end
-
       end
     end
   end

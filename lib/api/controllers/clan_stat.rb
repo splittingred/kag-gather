c14e81bf2f9641f07f02cf2398fb286f3ca2ebc7
Shaun McCormick <shaun@modx.com>
2013-04-29 22:07:31 -0500

    clan stats

---
+++ lib/api/controllers/clan_stat.rb
@@ -0,0 +1,34 @@
+require 'api/controllers/base'
+module KAG
+  module API
+    module Controller
+      class Stat < Base
+        @@class_key = 'ClanStat'
+        @@primary_key = :name
+
+        def get
+          name = @params[:name].to_s || ''
+          if name.empty?
+            self.failure('err_ns')
+          else
+            limit = @params[:limit] || 20
+            times = ::ClanStat.where(:name => name).sum('value')
+            clans_count = ::ClanStat.select('*,clans.name').joins(:clan).where(:clan_stats => {:name => name}).count
+            clans = ::ClanStat.select('*,clans.name').joins(:clan).where(:clan_stats => {:name => name}).limit(limit).order('value DESC')
+            list = []
+            clans.each do |c|
+              list << {
+                  :clan => c.name,
+                  :times => c.value
+              }
+            end
+            self.collection(list,clans_count,{
+                :name => name,
+                :times => times,
+            })
+          end
+        end
+      end
+    end
+  end
+end

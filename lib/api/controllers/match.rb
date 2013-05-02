4dbbf5cebd468f20ab659a443b0e64cfa98cbf81
Shaun McCormick <shaun@modx.com>
2013-05-02 12:55:25 -0500

    match-specific stats

--- lib/api/controllers/match.rb
+++ lib/api/controllers/match.rb
@@ -13,6 +13,9 @@ module KAG
               data[:stats] = JSON.parse(match.stats)
               data[:stats].delete(:units_depleted)
             end
+            if match.server
+              data[:server_name] = match.server.name
+            end
             data[:players] = []
             match.users.each do |u|
               data[:players] << u.name
@@ -23,6 +26,24 @@ module KAG
             self.failure('err_nf',id)
           end
         end
+
+        def list
+          limit = @params[:limit] || 10
+          offset = @params[:offset] || 0
+          c = ::Match.select('`matches`.*,`servers`.`name` AS `server_name`').joins(:server).where('matches.end_votes = 0 AND matches.ended_at IS NOT NULL').limit(limit).offset(offset).order('matches.created_at DESC')
+          if c
+            us = []
+            c.each do |u|
+              ud = SymbolTable.new(u.attributes)
+              ud.delete(:stats)
+              us << ud
+            end
+            self.success('',us)
+          else
+            self.failure('err_nf',c)
+          end
+        end
+
       end
     end
   end

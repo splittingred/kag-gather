91e2f7b07b56ca70d4944c53c29a864bee183b6c
Shaun McCormick <shaun@modx.com>
2013-05-18 14:06:15 -0500

    top achievers rankings

---
+++ lib/api/controllers/achievers.rb
@@ -0,0 +1,29 @@
+require 'api/controllers/base'
+module KAG
+  module API
+    module Controller
+      class Achievers < Base
+        @@class_key = 'User'
+        @@primary_key = :kag_user
+
+        def get
+          self.list
+        end
+        def list
+          achievements = ::UserAchievement.select('COUNT(*) AS `achievements`, `users`.`kag_user`').joins('JOIN users ON users.id = user_achievements.user_id').where('users.kag_user != ""').group('user_achievements.user_id').order('achievements DESC').limit(25)
+          if achievements
+            list = []
+            achievements.each do |achievement|
+              data = SymbolTable.new(achievement.attributes)
+              list << data
+            end
+            self.collection(list,list.count)
+          else
+            self.failure('err_nf',c)
+          end
+        end
+
+      end
+    end
+  end
+end
